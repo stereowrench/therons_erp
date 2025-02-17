20f591c139da467be6fc6740c662dcee1a78d18b
Theron Boerner <hunterboerner@gmail.com>
2025-02-17 18:27:31 -0500

    Add stubs for sales order test

--- test/therons_erp_web/live/sales_orders_live.exs
+++ test/therons_erp_web/live/sales_orders_live.exs
@@ -1,3 +1,9 @@
 defmodule TheronsErpWeb.SalesOrdersLiveTest do
   use TheronsErpWeb.ConnCase
+
+  test "Sales order can be created"
+  test "Sales order customer and address"
+  test "Sales order line items"
+  test "Sales order draft -> ready"
+  test "Sales order ready -> invoice"
 end
