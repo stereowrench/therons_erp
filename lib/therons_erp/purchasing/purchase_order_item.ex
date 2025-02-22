232fe5ec300b121cbe90893ac74d74fa9a7144d8
Theron Boerner <hunterboerner@gmail.com>
2025-02-22 10:28:21 -0500

    Work on PO

--- lib/therons_erp/purchasing/purchase_order_item.ex
+++ lib/therons_erp/purchasing/purchase_order_item.ex
@@ -2,11 +2,21 @@ defmodule TheronsErp.Purchasing.PurchaseOrderItem do
   use Ash.Resource,
     otp_app: :therons_erp,
     domain: TheronsErp.Purchasing,
-    data_layer: AshPostgres.DataLayer
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshStateMachine]
 
   postgres do
     table "purchase_order_items"
     repo TheronsErp.Repo
+  end
+
+  state_machine do
+    initial_states([:draft])
+    default_initial_state(:draft)
+
+    transitions do
+      # transition(:)
+    end
   end
 
   attributes do
