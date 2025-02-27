ea428dff39c0e111f21918acdaecbba0c21cfe75
Theron Boerner <hunterboerner@gmail.com>
2025-02-27 12:49:53 -0500

    Use PG for Oban notifier

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ import Config
 
 config :therons_erp, Oban,
   engine: Oban.Engines.Basic,
-  notifier: Oban.Notifiers.Postgres,
+  notifier: Oban.Notifiers.PG,
   queues: [default: 10],
   repo: TheronsErp.Repo
 
